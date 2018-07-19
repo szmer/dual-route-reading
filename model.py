6d10b6282fde1a2e8963c46f18f8fe7e8dbd9f16
Szymon Rutkowski <szymon@szymonrutkowski.pl>
2018-07-20 01:22:48 +0200

    graphemic and lexical route simultaneously

--- model.py
+++ model.py
@@ -11,14 +11,15 @@ prm = {
         'letter_neuron_params_on': { 'I_e': 900.0 }, # constant input current in pA
         'letter_column_size': 4,
         'head_column_size': 12,
-        'channel_column_size': 8,
         'grapheme_column_size': 8,
+        'lexical_column_size': 12,
         # Synapse specifications.
         'letter_col_lateral_inhibition': { 'weight': -100.0 },
         'letter_head_excitation': { 'weight': 300.0 },
-        'member_letter_excitation': { 'weight': 0.0 },#4.0 },
-        'absent_letter_inhibition': { 'weight': 0.0 },#-4.0 },
-        'shorter_word_inhibition': { 'weight': 0.0 },#-1.2 },
+        'member_letter_excitation': { 'weight': 70.0 },
+        'absent_letter_inhibition': { 'weight': -40.0 },
+        'shorter_word_inhibition': { 'weight': -12.0 },
+        'lexical_grapheme_excitation': { 'weight': 35.0 },
         # weights letter -> heights are divided by (1 + (grapheme_len-1)*this)
         'grapheme_length_damping': 0.8,
         'head_grapheme_synapse_model': { 'U': 0.67, 'u': 0.67, 'x': 1.0, 'tau_rec': 50.0,
@@ -42,8 +43,28 @@ prm = {
             'zet',
             'zest' ]
         }
+
 graphemes_by_lengths = [[g for g in prm['graphemes'] if len(g) == l]
                         for l in range(max([len(g) for g in prm['graphemes']])+1)]
+
+def decompose_word(word):
+    "Get a list of graphemes in the word."
+    current_pos = 0
+    graphemes = []
+    while current_pos < len(word):
+        found = False
+        for len_graphemes in reversed(graphemes_by_lengths):
+            for grapheme in len_graphemes:
+                if word[current_pos:current_pos+len(grapheme)] == grapheme:
+                    graphemes.append(grapheme)
+                    found = True
+                    break
+            if found:
+                break
+        else:
+            raise ValueError('Cannot decompose string {} in word {}'.format(word[current_pos:], word))
+        current_pos += len(graphemes[-1])
+    return graphemes
 
 # Parse command line args.
 if not (len(sys.argv) in [2, 3]):
@@ -69,7 +90,8 @@ nest.CopyModel('tsodyks2_synapse', 'head_grapheme_synapse_model', prm['head_grap
 def make_hypercolumn(stimuli_set, column_size):
     return dict([(s, nest.Create(prm['neuron_type'], column_size)) for s in stimuli_set])
 
-lexical_cols = dict([(w, nest.Create(prm['neuron_type'])) for w in prm['vocabulary']])
+lexical_cols = dict([(w, nest.Create(prm['neuron_type'], prm['lexical_column_size']))
+                     for w in prm['vocabulary']])
 letter_hypercolumns = [make_hypercolumn(prm['letters'], prm['letter_column_size'])
                        for i in range(prm['max_text_len'])]
 # Reading heads' columns are sorted in separate lists by grapheme lengths.
@@ -111,14 +133,21 @@ for len_graphemes in reading_head:
     for (grapheme, grapheme_col) in len_graphemes.items():
         nest.Connect(grapheme_col,
                      sum([list(neurs)
-                          for col in grapheme_hypercolumns
-                          for (label, neurs) in col.items()
+                          for hypercol in grapheme_hypercolumns
+                          for (label, neurs) in hypercol.items()
                           if label == grapheme], []),
                      syn_spec='head_grapheme_synapse_model')
+for (word, word_col) in lexical_cols.items():
+    word_decomposition = decompose_word(word)
+    for (hcol_n, hypercol) in enumerate(grapheme_hypercolumns):
+        if hcol_n == len(word_decomposition):
+            break
+        nest.Connect(word_col, hypercol[word_decomposition[hcol_n]],
+                     syn_spec=prm['lexical_grapheme_excitation'])
 
 # Insert probes:
-###for (word, word_col) in lexical_cols.items():
-###    insert_probe(word_col, word)
+for (word, word_col) in lexical_cols.items():
+    insert_probe(word_col, word)
 for (letter, letter_col) in letter_hypercolumns[1].items():
     insert_probe(letter_col, 'L2-'+letter)
 for len_graphemes in reading_head:
@@ -128,6 +157,8 @@ for (grapheme, grapheme_col) in grapheme_hypercolumns[0].items():
     insert_probe(grapheme_col, 'G1-'+grapheme)
 for (grapheme, grapheme_col) in grapheme_hypercolumns[1].items():
     insert_probe(grapheme_col, 'G2-'+grapheme)
+for (grapheme, grapheme_col) in grapheme_hypercolumns[2].items():
+    insert_probe(grapheme_col, 'G3-'+grapheme)
 
 # Run the simulation, write readings.
 nest.Simulate(prm['letter_focus_time'])
